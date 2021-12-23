a28e0cfd30a29cb3dacd6aa60d4736d3a5c4d02e
Caleb Lehman <lehman.346@osu.edu>
2021-12-23 15:58:20 -0500

    Add solution for day 23

---
+++ year-2021/day-23/main-2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# didn't really want to write yet another graph search; seemed easier to work out by hand
+# (especially since the order of magnitude difference between costs makes an easy heuristic)
+
+step_file='main-2.txt'
+a=$(grep 'cost.*A' ${step_file} | cut -d' ' -f2 | xargs | sed -e 's/\ /+/g' | bc)
+b=$(grep 'cost.*B' ${step_file} | cut -d' ' -f2 | xargs | sed -e 's/\ /+/g' | bc)
+c=$(grep 'cost.*C' ${step_file} | cut -d' ' -f2 | xargs | sed -e 's/\ /+/g' | bc)
+d=$(grep 'cost.*D' ${step_file} | cut -d' ' -f2 | xargs | sed -e 's/\ /+/g' | bc)
+
+echo "${a} + 10 * ${b} + 100 * ${c} + 1000 * ${d}" | bc
